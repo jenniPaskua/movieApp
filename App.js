bbfc075377f5e7abc8e127dee5db62c93f311bbf
Jenni <jenniPaskua@gmail.com>
2022-05-14 08:20:51 +0900

    ✨ swiper 추가, typescript adapt

--- App.js
+++ App.js
@@ -1,21 +1,26 @@
 import AppLoading from "expo-app-loading";
 import React, { useState } from "react";
-import { Text, Image } from "react-native";
+import { Text, Image, useColorScheme } from "react-native";
 import * as Font from "expo-font";
 import { Ionicons } from "@expo/vector-icons";
 import { Asset, useAssets } from "expo-asset";
 import { NavigationContainer } from "@react-navigation/native";
 import Root from "./navigation/Root";
+import { ThemeProvider } from "styled-components/native";
+import { darkTheme, lightTheme } from "./styled";
 
 export default function App() {
   const [assets] = useAssets([require("./me.jpeg")]);
   const [loaded] = Font.useFonts(Ionicons.font);
+  const isDark = useColorScheme() === "dark";
   if (!assets || !loaded) {
     return <AppLoading />;
   }
   return (
-    <NavigationContainer>
-      <Root />
-    </NavigationContainer>
+    <ThemeProvider theme={isDark ? darkTheme : lightTheme}>
+      <NavigationContainer>
+        <Root />
+      </NavigationContainer>
+    </ThemeProvider>
   );
 }
