4ed973a597612940dc94cd419ae827d61bede7e3
Jenni <jenniPaskua@gmail.com>
2022-05-14 18:51:21 +0900

    💄 stack & tabs 수정

--- navigation/Stack.tsx
+++ navigation/Stack.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { Text, TouchableOpacity } from "react-native";
+import { Text, TouchableOpacity, useColorScheme } from "react-native";
+import { BLACK_COLOR } from "../colors";
 
 const ScreenOne = ({ navigation: { navigate } }) => (
   <TouchableOpacity onPress={() => navigate("Two")}>
@@ -20,12 +21,26 @@ const ScreenThree = ({ navigation: { navigate } }) => (
 
 const NativeStack = createNativeStackNavigator();
 
-const Stack = () => (
-  <NativeStack.Navigator>
-    <NativeStack.Screen name='One' component={ScreenOne} />
-    <NativeStack.Screen name='Two' component={ScreenTwo} />
-    <NativeStack.Screen name='Three' component={ScreenThree} />
-  </NativeStack.Navigator>
-);
+const Stack = () => {
+  const isDark = useColorScheme() == "dark";
+
+  return (
+    <NativeStack.Navigator
+      screenOptions={{
+        headerBackTitleVisible: false,
+        headerStyle: {
+          backgroundColor: isDark ? BLACK_COLOR : "white",
+        },
+        headerTitleStyle: {
+          color: isDark ? "white" : BLACK_COLOR,
+        },
+      }}
+    >
+      <NativeStack.Screen name='One' component={ScreenOne} />
+      <NativeStack.Screen name='Two' component={ScreenTwo} />
+      <NativeStack.Screen name='Three' component={ScreenThree} />
+    </NativeStack.Navigator>
+  );
+};
 
 export default Stack;
--- navigation/Tabs.tsx
+++ navigation/Tabs.tsx
@@ -5,58 +5,65 @@ import Search from "../screens/Search";
 import Tv from "../screens/Tv";
 import { Ionicons } from "@expo/vector-icons";
 import { BLACK_COLOR, DARK_GREY, WHITE, YELLOW_COLOR } from "../colors";
+import { useColorScheme } from "react-native";
 
 const Tab = createBottomTabNavigator();
 
-const Tabs = () => (
-  <Tab.Navigator
-    screenOptions={{
-      tabBarStyle: {
-        backgroundColor: BLACK_COLOR,
-      },
-      tabBarActiveTintColor: YELLOW_COLOR,
-      tabBarInactiveTintColor: DARK_GREY,
-      headerStyle: {
-        backgroundColor: BLACK_COLOR,
-      },
-      headerTitleStyle: {
-        color: WHITE,
-      },
-      tabBarlabelStyle: {
-        marginTop: -5,
-        fontSize: 10,
-        fontWeight: "600",
-      },
-    }}
-  >
-    <Tab.Screen
-      name='Movies'
-      component={Movies}
-      options={{
-        tabBarIcon: ({ color, size }) => (
-          <Ionicons name={"film-outline"} color={color} size={size} />
-        ),
+const Tabs = () => {
+  const isDark = useColorScheme() === "dark";
+  return (
+    <Tab.Navigator
+      sceneContainerStyle={{
+        backgroundColor: isDark ? BLACK_COLOR : "white",
       }}
-    />
-    <Tab.Screen
-      name='TV'
-      component={Tv}
-      options={{
-        tabBarIcon: ({ color, size }) => (
-          <Ionicons name='tv-outline' color={color} size={size} />
-        ),
+      screenOptions={{
+        tabBarStyle: {
+          backgroundColor: BLACK_COLOR,
+        },
+        tabBarActiveTintColor: YELLOW_COLOR,
+        tabBarInactiveTintColor: DARK_GREY,
+        headerStyle: {
+          backgroundColor: BLACK_COLOR,
+        },
+        headerTitleStyle: {
+          color: WHITE,
+        },
+        tabBarlabelStyle: {
+          marginTop: -5,
+          fontSize: 10,
+          fontWeight: "600",
+        },
       }}
-    />
-    <Tab.Screen
-      name='Search'
-      component={Search}
-      options={{
-        tabBarIcon: ({ color, size }) => (
-          <Ionicons name={"search-outline"} color={color} size={size} />
-        ),
-      }}
-    />
-  </Tab.Navigator>
-);
+    >
+      <Tab.Screen
+        name='Movies'
+        component={Movies}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name={"film-outline"} color={color} size={size} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name='TV'
+        component={Tv}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name='tv-outline' color={color} size={size} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name='Search'
+        component={Search}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name={"search-outline"} color={color} size={size} />
+          ),
+        }}
+      />
+    </Tab.Navigator>
+  );
+};
 
 export default Tabs;
