79016002d921b33ba8eaf036bb8275fb268b1e73
Jenni <jenniPaskua@gmail.com>
2022-05-14 15:36:19 +0900

    ✨ rn-swiper 추가, now_playing fetch 추가

--- screens/Movies.tsx
+++ screens/Movies.tsx
@@ -1,40 +1,129 @@
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
-import react from "react";
-import { TouchableOpacity, Text } from "react-native";
-import { Dimensions } from "react-native";
-import Swiper from "react-native-web-swiper";
+import React, { useEffect, useState } from "react";
+import { TouchableOpacity, Text, ScrollView } from "react-native";
+import {
+  ActivityIndicator,
+  Dimensions,
+  StyleSheet,
+  Appearance,
+} from "react-native";
 import styled from "styled-components/native";
+import { BlurView } from "expo-blur";
+import { makeImgPath } from "../utils";
+import { useColorScheme } from "react-native";
+import Swiper from "react-native-swiper";
 
 const API_KEY = "b1abc7c7e5978de88700bb4fc535fafc";
 
-const Container = styled.ScrollView`
-  background-color: ${(props) => props.theme.mainBgColor};
-`;
+const Container = styled.ScrollView``;
 
 const View = styled.View`
   flex: 1;
 `;
 
+const Loader = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+`;
+
+const BgImg = styled.Image``;
+
+const Poster = styled.Image`
+  width: 100px;
+  height: 160px;
+  border-radius: 5px;
+`;
+
+const Title = styled.Text<{ isDark: boolean }>`
+  font-size: 16px;
+  font-weight: 600;
+  color: ${(props) => (props.isDark ? "white" : "red")};
+`;
+
+const Wrapper = styled.View`
+  flex-direction: row;
+  height: 100%;
+  width: 90%;
+  margin: 0 auto;
+  justify-content: space-around;
+  align-items: center;
+`;
+
+const Column = styled.View`
+  width: 60%;
+`;
+
+const Overview = styled.Text<{ isDark: boolean }>`
+  margin-top: 10px;
+  color: ${(props) =>
+    props.isDark ? "rgba(255, 255, 255, 0.8)" : "rgba(0, 0, 0, 0.8)"};
+  }
+`;
+const Votes = styled(Overview)`
+  font-size: 12px;
+`;
+
 const { height: SCREEN_HEIGHT } = Dimensions.get("window");
 
 const Movies: React.FC<NativeStackScreenProps<any, "Movies">> = () => {
-  const getNowPlaying = () => {
-    fetch(
-      `https://api.themoviedb.org/3/movie/now_playing?api_key=${API_KEY}&language=en-US&page=1&region=KR`
-    );
+  const isDark = useColorScheme() === "dark";
+  const [loading, setLoading] = useState(true);
+  const [nowPlaying, setNowPlaying] = useState([]);
+  const getNowPlaying = async () => {
+    const { results } = await (
+      await fetch(
+        `https://api.themoviedb.org/3/movie/now_playing?api_key=${API_KEY}&language=en-US&page=1`
+      )
+    ).json();
+    setNowPlaying(results);
+    setLoading(false);
   };
-  return (
+  useEffect(() => {
+    getNowPlaying();
+  }, []);
+
+  return loading ? (
+    <Loader>
+      <ActivityIndicator />
+    </Loader>
+  ) : (
     <Container>
       <Swiper
+        horizontal
         loop
-        timeout={3.5}
-        controlsEnabled={false}
+        autoplay
+        autoplayTimeout={3.5}
+        showsButtons={false}
+        showsPagination={false}
         containerStyle={{ width: "100%", height: SCREEN_HEIGHT / 4 }}
       >
-        <View style={{ backgroundColor: "red" }}></View>
-        <View style={{ backgroundColor: "blue" }}></View>
-        <View style={{ backgroundColor: "red" }}></View>
-        <View style={{ backgroundColor: "blue" }}></View>
+        {nowPlaying.map((movie) => (
+          <View key={movie.id}>
+            <BgImg
+              style={StyleSheet.absoluteFill}
+              source={{ uri: makeImgPath(movie.backdrop_path) }}
+            />
+            <BlurView
+              tint={isDark ? "dark" : "light"}
+              intensity={85}
+              style={StyleSheet.absoluteFill}
+            >
+              <Wrapper>
+                <Poster source={{ uri: makeImgPath(movie.poster_path) }} />
+                <Column>
+                  <Title isDark={isDark}>{movie.original_title}</Title>
+                  {movie.vote_average > 0 ? (
+                    <Votes isDark={isDark}>{movie.vote_average}/10</Votes>
+                  ) : null}
+                  <Overview isDark={isDark}>
+                    {movie.overview.slice(0, 100)}...
+                  </Overview>
+                </Column>
+              </Wrapper>
+            </BlurView>
+          </View>
+        ))}
       </Swiper>
     </Container>
   );
